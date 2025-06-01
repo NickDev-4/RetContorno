3b69f0c30ab1165b37cb02f3d01448b7cdf3d849
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-06-01 18:11:28 -0300

    Lista 5

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int altura, largura;
+
+Console.Write("Digite o tamanho da altura: ");
+altura = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o tamanho da largura: ");
+largura = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+for (int y = 0; y < altura; y++) {
+    for (int x = 0; x < largura; x++) {
+        if (y == 0 || y == altura - 1 || x == 0 || x == largura - 1) {
+            Console.Write("*");
+        }
+        else {
+            Console.Write(" ");
+        }
+    }
+    
+    Console.WriteLine();
+}
